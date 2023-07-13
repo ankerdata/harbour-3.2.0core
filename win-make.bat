5fab553f000e954af115b46d0fb821bad7a13874
Graham Strickland <grahamhstrickland@gmail.com>
2023-07-13 15:45:06 +0200

    Update win-make.bat for MSVC2022

--- win-make.bat
+++ win-make.bat
@@ -1,13 +1,13 @@
-rem call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
-set WindowsSdkDir=\Program Files\Microsoft SDKs\Windows\v6.0A\
-call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+rem call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+set WindowsSdkDir="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\"
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 set HB_COMPILER=msvc
 set HB_INSTALL_PREFIX=C:\Users\<username>\dev\harbour-3.2.0dev
 set HB_BUILD_MODE=c
 set HB_USER_PRGFLAGS=-l-
 set HB_BUILD_CONTRIBS=
 set HB_WITH_CURL=C:\Users\<username>\dev\harbour-3.2.0dev-libs\curl-7.54.0-win32-mingw\include
-set HB_WITH_FREEIMAGE=C:\Users\<username>\dev\harbour-3.2.0dev-libs\FreeImage3170Win32Win64\include
+set HB_WITH_FREEIMAGE=C:\Users\<username>\dev\harbour-3.2.0dev-libs\FreeImage3170Win32Win64\FreeImage\Dist\x32
 set HB_WITH_OPENSSL=C:\Users\<username>\dev\harbour-3.2.0dev-libs\OpenSSL-Win32\include
 set HB_STATIC_OPENSSL=yes
 set HB_STATIC_CURL=yes
