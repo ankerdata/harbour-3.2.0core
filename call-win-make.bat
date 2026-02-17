0fcb9ef0d2baa8a2043c02511f8622bb7354c506
Alex Strickland <sscc@mweb.co.za>
2026-02-17 11:47:58 +0200

    Add vcpkg paths, fix freeimage path and hbfimage.hbp to match

--- call-win-make.bat
+++ call-win-make.bat
@@ -9,25 +9,29 @@ if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\B
 )
 
 set HB_COMPILER=msvc
-rem Relative paths don't work
-rem set HB_INSTALL_PREFIX=..\..\harbour-3.2.0dev
 set HB_INSTALL_PREFIX=%USERPROFILE%\dev\harbour-3.2.0dev
 set HB_BUILD_MODE=c
 set HB_USER_PRGFLAGS=-l-
 set HB_BUILD_CONTRIBS=
 
-if "%CURL_PREFIX%"=="" set CURL_PREFIX=%USERPROFILE%\dev\harbour-3.2.0dev-libs\curl-7.54.0-win32-mingw
-set HB_WITH_CURL=%CURL_PREFIX%\include
+set VCPKG=%USERPROFILE%\dev\harbour-3.2.0dev-vcpkg\vcpkg\installed\x86-windows\include
+
+set HB_WITH_CURL=%VCPKG%
+set HB_WITH_OPENSSL=%VCPKG%
+
+rem This has about a million dependencies with vcpkg, so we will just disable it for now
+rem set HB_WITH_FREEIMAGE=%VCPKG%
 
 if "%FREEIMAGE_PREFIX%"=="" set FREEIMAGE_PREFIX=%USERPROFILE%\dev\harbour-3.2.0dev-libs\FreeImage3170Win32Win64\FreeImage
 set HB_WITH_FREEIMAGE=%FREEIMAGE_PREFIX%\Dist\x32
 
-if "%OPENSSL_PREFIX%"=="" set OPENSSL_PREFIX=%USERPROFILE%\dev\harbour-3.2.0dev-libs\OpenSSL-Win32
-set HB_WITH_OPENSSL=%OPENSSL_PREFIX%\include
-
 set HB_STATIC_OPENSSL=yes
 set HB_STATIC_CURL=yes
 
-win-make.exe clean install
+rem Clean build
+rem win-make.exe clean install > build.log 2>&1
+
+rem Changes only
+win-make.exe install > build.log 2>&1
 
 endlocal
