974a831cc42fc3ebc7f09930a66ad6cf49619963
Alex Strickland <sscc@mweb.co.za>
2026-04-06 22:13:39 +0200

    Script to build harbour with homebrew.

---
+++ makeharbour.sh
@@ -0,0 +1,9 @@
+export HB_WITH_ZLIB=/opt/homebrew/opt/zlib/include
+export HB_WITH_PCRE=/opt/homebrew/opt/pcre/include
+export HB_WITH_PNG=/opt/homebrew/opt/libpng/include
+export HB_WITH_CURL=/opt/homebrew/opt/curl/include
+export HB_WITH_JPEG=nolocal
+
+# make clean
+# make
+make install
